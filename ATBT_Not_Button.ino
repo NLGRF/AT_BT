a64b88fe70c66e4b4a8917c010789c33f8d06a26
NLGRF <nongrace6654@gmail.com>
2016-12-03 15:59:15 +0700

    ATBT_Not_Button

---
+++ ATBT_Not_Button.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(4, 5); // RX | TX
+
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("Enter AT commands:");
+
+    BTserial.begin(9600);  
+}
+ 
+void loop()
+{
+ 
+    if (BTserial.available())
+    {  
+        Serial.write(BTserial.read());
+    }
+    
+    if (Serial.available())
+    {
+        BTserial.write(Serial.read());
+    }
+ 
+}
